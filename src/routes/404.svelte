98873ee2fc0430cbaa6cc74099e8a38aa5a4f009
Jacob Larfors <jlarfors@verifa.io>
2022-02-16 13:16:25 +0200

    Add 404 page to redirect not found requests to

---
+++ src/routes/404.svelte
@@ -0,0 +1,7 @@
+<section>
+	<h1>
+		<span class="text-v-lilac">New website.</span>
+		<span class="text-v-pink">New routes.</span>
+	</h1>
+	<h3>We are sorry that you have landed here. Check the header for links to common pages.</h3>
+</section>
