51cf4985fcf2bec24e8a977dfeb5c98fe1ca9e9f
Jacob Larfors <jlarfors@verifa.io>
2022-04-05 13:43:26 +0300

    Add attachment to careers form

--- src/lib/careers/careersForm.svelte
+++ src/lib/careers/careersForm.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let attachments: string[] = [];
+
+	function handleAttachments(event) {
+		attachments = Object.values(event.target.files).map((file) => file['name']);
+	}
+
+	onMount(() => {
+		// Setup listener for when files are attached
+		const inputElement = document.getElementById('attachment');
+		inputElement.addEventListener('change', handleAttachments, false);
+	});
+</script>
+
 <div class="max-w-4xl">
 	<form
 		action="https://usebasin.com/f/9f1e7bc2e780"
@@ -40,6 +56,39 @@
 					placeholder="https://github.com/<profile>"
 					pattern="https://(www.)?github.com/.*"
 				/>
+			</div>
+		</div>
+		<div class="flex gap-x-4 items-center">
+			<div
+				class="self-start py-1 px-4 border-2 border-v-lilac bg-v-lilac text-v-white  hover:bg-violet-400"
+			>
+				<label for="attachment" class="cursor-pointer font-medium text-lg">
+					<div class="flex items-center gap-x-1">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+							/>
+						</svg>
+						<span>Attach files</span>
+					</div>
+				</label>
+				<input type="file" id="attachment" name="attachments[]" class="hidden" multiple />
+			</div>
+			<div class="-my-2 flex flex-wrap gap-x-4">
+				{#each attachments as attachment}
+					<span class="inline-flex items-center my-2 px-3 py-0.5 bg-v-gray">
+						<p class="m-0 text-v-white">{attachment}</p>
+					</span>
+				{/each}
 			</div>
 		</div>
 		<div class="flex gap-x-4 items-center">
