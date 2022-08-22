c926c1145d67e431c9844ab2a9deca8677710394
Jacob Lärfors <jlarfors@verifa.io>
2022-08-22 10:03:22 +0300

    Move header permalinks to end of text
    Just prefer it being at the end of the text and easier from the CSS perspective to reuse.

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -22,8 +22,8 @@ const config = {
 		[
 			rehypeAutolinkHeadings,
 			{
-				// Prepend link to header
-				behavior: 'prepend',
+				// Append link to header
+				behavior: 'append',
 				properties: {
 					// Add class to <a> element
 					class: 'group header-anchor-link'
@@ -32,25 +32,23 @@ const config = {
 					// Add class to each header
 					node.properties.class = 'group header-anchor'
 					return [
+						h('span.hidden', ' permalink'),
 						// Add link svg from heroicons
-						h(
-							'span',
-							s('svg', {
-								xmlns: 'http://www.w3.org/2000/svg',
-								// Add custom classes, including header-anchor-icon-hx depending on size of header
-								class: `header-anchor-icon header-anchor-icon-${node.tagName}`,
-								fill: "none",
-								viewBox: "0 0 24 24",
-								stroke: "currentColor",
-								'stroke-width': "2"
-							}, [
-								s('path', {
-									'stroke-linecap': "round",
-									'stroke-linejoin': "round",
-									d: "M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-								})
-							])
-						),
+						s('svg', {
+							xmlns: 'http://www.w3.org/2000/svg',
+							// Add custom classes, including header-anchor-icon-hx depending on size of header
+							class: `header-anchor-icon header-anchor-icon-${node.tagName}`,
+							fill: "none",
+							viewBox: "0 0 24 24",
+							stroke: "currentColor",
+							'stroke-width': "2"
+						}, [
+							s('path', {
+								'stroke-linecap': "round",
+								'stroke-linejoin': "round",
+								d: "M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+							})
+						])
 					]
 
 				}
