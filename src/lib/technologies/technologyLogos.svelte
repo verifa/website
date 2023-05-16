f21033bcfbd061f7aed66c21999cbc3a478266c1
XCompWiz <xcompwiz@gmail.com>
2023-05-16 16:22:54 +0300

    Add Kubernetes image and adjust tech logos

--- src/lib/technologies/technologyLogos.svelte
+++ src/lib/technologies/technologyLogos.svelte
@@ -23,7 +23,7 @@
 	{#each logos as logo, index}
 		{#if showIndex(index)}
 			<div class="col-span-3 flex justify-center md:col-span-3 lg:col-span-3 py-8 px-8 bg-v-lilac">
-				<img src={logo.image} alt={logo.name} class="h-8 w-full object-contain" />
+				<img src={logo.image} alt={logo.name} class="h-12 w-full object-contain" />
 			</div>
 		{/if}
 	{/each}
