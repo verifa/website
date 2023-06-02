43bc05eadb08d48faaffaa0e840fb446f0c86ecf
XCompWiz <xcompwiz@gmail.com>
2023-06-02 13:17:05 +0300

    Enable tweaking buttonLink and shrink buttons on main page on mobile

--- src/lib/buttonLink.svelte
+++ src/lib/buttonLink.svelte
@@ -1,11 +1,13 @@
 <script>
 	export let href;
+	export { className as class };
+	let className = '';
 </script>
 
 <div class="my-5">
 	<a
 		{href}
-		class="inline-flex px-5 py-3 border border-transparent text-v-white bg-v-black hover:bg-v-lilac"
+		class="inline-flex px-5 py-3 border border-transparent text-v-white bg-v-black hover:bg-v-lilac {className}"
 	>
 		<slot />
 	</a>
