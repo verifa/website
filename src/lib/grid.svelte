ab00ba0c8648aca25206ab42d37beefebb52f3e3
XCompWiz <xcompwiz@gmail.com>
2023-06-13 15:34:48 +0300

    Change Grid to top align items (not center) Only impactful on landing page; everywhere else has even unit items

--- src/lib/grid.svelte
+++ src/lib/grid.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="mx-auto max-w-xl lg:max-w-none grid grid-cols-1 lg:grid-cols-2 items-center gap-y-8 md:gap-y-16 lg:gap-y-24 lg:px-8 lg:gap-x-24 {className}"
+	class="mx-auto max-w-xl lg:max-w-none grid grid-cols-1 lg:grid-cols-2 gap-y-8 md:gap-y-16 lg:gap-y-24 lg:px-8 lg:gap-x-24 {className}"
 >
 	<slot />
 </div>
