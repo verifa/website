f60aa8fedbb276879e67c5ce7032bc5ee66ef53a
Jacob Lärfors <1135394+jlarfors@users.noreply.github.com>
2024-04-05 09:54:26 +0300

    add gha oidc trust with gcp workload identity

---
+++ infra/ghoidc.tf
@@ -0,0 +1,27 @@
+# Configure GitHub OIDC in Google Cloud to allow keyless access to GCP from GHA.
+
+resource "google_service_account" "gha" {
+  project    = var.project
+  account_id = "verifa-website-gha"
+}
+
+resource "google_project_iam_member" "gha" {
+  project = var.project
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.gha.email}"
+}
+
+
+module "gh_oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  version     = "3.1.2"
+  project_id  = var.project
+  pool_id     = "verifa-website-pool"
+  provider_id = "verifa-website-gh-provider"
+  sa_mapping = {
+    (google_service_account.gha.account_id) = {
+      sa_name   = google_service_account.gha.name
+      attribute = "attribute.repository/verifa/website"
+    }
+  }
+}
