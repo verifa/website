e18fb3b15aa806ab628ac877810c8bb43b605400
Jacob Lärfors <1135394+jlarfors@users.noreply.github.com>
2024-04-05 09:54:26 +0300

    add gha oidc trust with gcp workload identity

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,11 @@ output "cloud_run_staging" {
 output "cloud_run_prod" {
   value = module.prod-service.service_name
 }
+
+output "gcp_service_account" {
+  value = google_service_account.gha.email
+}
+
+output "gcp_workload_identity_provider" {
+  value = module.gh_oidc.provider_name
+}
